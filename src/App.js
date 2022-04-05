a7f1b0f371761ef61c5dd225162fa00a8b8316b2
abhinav-22-tech <jainabhinav0860@gmail.com>
2022-04-05 14:46:28 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FaceMesh } from "@mediapipe/face_mesh";
+import React, { useRef, useEffect } from "react";
+import * as Facemesh from "@mediapipe/face_mesh";
+import * as cam from "@mediapipe/camera_utils";
+import Webcam from "react-webcam";
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const connect = window.drawConnectors;
+  var camera = null;
+  function onResults(results) {
+    // const video = webcamRef.current.video;
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        connect(canvasCtx, landmarks, Facemesh.FACE_GEOMETRY, {
+          color: "#eec1ad",
+          lineWidth: 1,
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, {
+          color: "#E0E0E0",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, {
+          color: "#e35d6a",
+        });
+      }
+    }
+    canvasCtx.restore();
+  }
+  // }
+
+  // setInterval(())
+  useEffect(() => {
+    const faceMesh = new FaceMesh({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+      },
+    });
+
+    faceMesh.setOptions({
+      maxNumFaces: 2,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    faceMesh.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await faceMesh.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="App">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "660",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            // textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          className="output_canvas"
+          style={{
+            position: "absolute",
+            marginLeft: "0",
+            marginRight: "0",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></canvas>
+      </div>
+    </center>
   );
 }
 
